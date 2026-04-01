c094a2bcb90363969df95f42eec609a84a90e77b
Ammar2380 <ammar211132@gmail.com>
2026-04-01 20:32:23 +0500

    complete list related tasks

--- New folder/List_task1.py
+++ New folder/List_task1.py
@@ -1,4 +1,5 @@
 l = []
+
 a = int(input("how many friends do u have: "))
 for i in range(a):
     one  = input("enter your freind name: ")
@@ -8,20 +9,19 @@ for i in range(a):
 print(l)
 for k in range(a):
     check = input("if u want to chek add your frend name: ")
-    check_father = input("if u want to chek add your frend father name: ")
-    if check == l & check_father == l:
-        print(f'yes you have that frend {check}{check_father}')
+    if check == l:
+        print("yes you have that frend")
     elif check == 'no':
         break
     else:
         add_new = input("this person is not in u r freind list do u want to add this frend: ")
         if add_new == 'yes':
                 l.append(check)
-                l.append(check_father)
                 print("congrats u have added new frend.....")
                 print(l)
         elif add_new == 'no':
                 break
-    
+a = tuple(l)
+print(a)
 
 
