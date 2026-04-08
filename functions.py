6b4d5d06f94279928515831e19d4ac39a134f9d1
Ammar2380 <ammar211132@gmail.com>
2026-04-08 22:42:32 +0500

    tasks

--- functions.py
+++ functions.py
@@ -5,31 +5,38 @@
 #     print(a+b)
 # myfunction(3,2) # arguments
 
-def sum_function(a,b):
+# def sum_function(a,b):
 
-    print(a+b)
+#     print(a+b)
  
-def sub_function(a,b):
+# def sub_function(a,b):
  
-    print(a-b)
+#     print(a-b)
  
-def mul_function(a,b):
+# def mul_function(a,b):
   
-    print(a*b)
+#     print(a*b)
  
-def div_function(a,b):
+# def div_function(a,b):
    
-    print(a/b)
+#     print(a/b)
 
-number1 = int(input("enter your number1: "))
-number2 = int(input("enter your number2: "))
-operation = input("enter your operation: ")
+# number1 = int(input("enter your number1: "))
+# number2 = int(input("enter your number2: "))
+# operation = input("enter your operation: ")
 
-if operation == '+':
-    sum_function(number1,number2)
-elif operation == '-':
-    sub_function(number1,number2)
-elif operation == "*":
-    mul_function(number1,number2)
-elif operation == "/":
-    div_function(number1,number2)
+# if operation == '+':
+#     sum_function(number1,number2)
+# elif operation == '-':
+#     sub_function(number1,number2)
+# elif operation == "*":
+#     mul_function(number1,number2)
+# elif operation == "/":
+#     div_function(number1,number2)
+
+
+books = ['English','science','gernal knowledge']
+
+
+
+user = input("if u want to borrow book press 1 chek 2 return 3")
